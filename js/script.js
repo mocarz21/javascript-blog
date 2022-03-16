e9a7c4ed03423a3f764413411b7a640ae1efe2c8
Adam Mocarski <adammocarski22@gmail.com>
2022-03-16 16:31:08 +0100

    Add Eslint fix code

--- js/script.js
+++ js/script.js
@@ -1,81 +1,74 @@
 'use strict';
 const titleClickHandler = function(event){
-    event.preventDefault();
-    const clickedElement = this;
-    console.log(event)
+  event.preventDefault();
+  const clickedElement = this;
+  console.log(event);
 
-    /* [Done] remove class 'active' from all article links  */
+  /* [Done] remove class 'active' from all article links  */
 
-    const activeLinks=document.querySelectorAll('.titles a');
+  const activeLinks=document.querySelectorAll('.titles a');
 
-    for(let activeLink of activeLinks )
-        activeLink.classList.remove('active');
+  for(let activeLink of activeLinks )
+    activeLink.classList.remove('active');
 
-    /* [Done] add class 'active' to the clicked link */
+  /* [Done] add class 'active' to the clicked link */
 
-    clickedElement.classList.add('active');
+  clickedElement.classList.add('active');
 
-    /* [Done] remove class 'active' from all articles */
-   const activeArticles=document.querySelectorAll('.posts article.active');
+  /* [Done] remove class 'active' from all articles */
 
+  const activeArticles=document.querySelectorAll('.posts article.active');
   for(let activeArticle of activeArticles)
     activeArticle.classList.remove('active');
 
-    /* [Done] get 'href' attribute from the clicked link */
+  /* [Done] get 'href' attribute from the clicked link */
 
   let atrybut = clickedElement.getAttribute('href');
-  console.log('atrybut: ', atrybut)
 
-    /*[Done] find the correct article using the selector (value of 'href' attribute) */
-  const correctArticle = document.querySelector(atrybut)
-  console.log('atrybut: ', correctArticle)
+  /*[Done] find the correct article using the selector (value of 'href' attribute) */
 
+  const correctArticle = document.querySelector(atrybut);
 
-    /* [Done] add class 'active' to the correct article */
+  /* [Done] add class 'active' to the correct article */
+
   correctArticle.classList.add('active');
-
-}
-
+};
 const links = document.querySelectorAll('.titles a');
-console.log('co: ', links)
+console.log('co: ', links);
 
 for(let link of links)
-    link.addEventListener('click', titleClickHandler);
+  link.addEventListener('click', titleClickHandler);
 
 function generateTitleLinks(){
-    /* [Done]remove contents of titleList */
-    const titleSelector= document.querySelector('.titles');
-        titleSelector.innerHTML='';
+  /* [Done]remove contents of titleList */
+  const titleSelector= document.querySelector('.titles');
+  titleSelector.innerHTML='';
         
+  /* for each article */
+  /* get the article id */
+  /* find the title element */
+  /* get the title from the title element */
+  const articles= document.querySelectorAll('.post');
 
-    /* for each article */
-    /* get the article id */
-    /* find the title element */
-    /* get the title from the title element */
-    const articles= document.querySelectorAll('.post')
-
-    for(let article of articles){
-        const articleId= article.getAttribute('id');
-
-        const titleFind=article.querySelector('.post-title')
-        let titlename= titleFind.innerHTML;
+  for(let article of articles){
+    const articleId= article.getAttribute('id');
+    const titleFind=article.querySelector('.post-title');
+    let titlename= titleFind.innerHTML;
         
-    
     /* create HTML of the link */
 
-        const html ='<li><a href=#'+ articleId +'"><span>'+ titlename + '</span></a></li>'
-        
-    
+    const html ='<li><a href=#'+ articleId +'"><span>'+ titlename + '</span></a></li>';
 
     /* insert link into titleList */
         
-        titleSelector.innerHTML= titleSelector + html; 
+    titleSelector.innerHTML= titleSelector + html;
 
     /*
-        titleSelector.insertAdjacentHTML("beforeend", html);
+    titleSelector.insertAdjacentHTML("beforeend", html);
     */
 
-        console.log('titlelist: ', titleSelector);
+    console.log('titlelist: ', titleSelector);
 
-    }
+  }
 }
+generateTitleLinks();
